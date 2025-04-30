df8b1edc47dbdb121a0cc69fe9263f66e3bd220c
palutz <stefano13@duck.com>
2025-04-30 17:57:26 +0100

    da1 task1, skiplist memtable

--- mini-lsm-starter/src/mem_table.rs
+++ mini-lsm-starter/src/mem_table.rs
@@ -15,9 +15,9 @@
 #![allow(unuseariables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use core::slice::SlicePattern;
-use std::borrow::Borrow;
-use std::ops::Bound;
+//use core::slice::SlicePattern;
+//use std::borrow::Borrow;
+//use std::ops::Bound;
 use std::path::Path;
 use std::sync::Arc;
 use std::sync::atomic::AtomicUsize;
@@ -25,9 +25,9 @@ use std::sync::atomic::AtomicUsize;
 use anyhow::Result;
 use bytes::{Buf, BufMut, Bytes};
 use crossbeam_skiplist::SkipMap;
-use nom::AsBytes;
+//use nom::AsBytes;
 use ouroboros::self_referencing;
-use rand::seq::SliceRandom;
+//use rand::seq::SliceRandom;
 
 use crate::iterators::StorageIterator;
 use crate::key::KeySlice;
@@ -56,18 +56,34 @@ pub(crate) fn map_bound(bound: Bound<&[u8]>) -> Bound<Bytes> {
 
 impl MemTable {
     /// Create a new mem-table.
-    pub fn create(_id: usize) -> Self {
-        unimplemented!()
+    pub fn create(id: usize) -> Self {
+        MemTable {
+            map : Arc::new(SkipMap::new()),
+            wal : None,
+            id,
+            approximate_size: Arc::new(AtomicUsize::new(0)),
+        }
     }
 
     /// Create a new mem-table with WAL
-    pub fn create_with_wal(_id: usize, _path: impl AsRef<Path>) -> Result<Self> {
-        unimplemented!()
+    pub fn create_with_wal(id: usize, path: impl AsRef<Path>) -> Result<Self> {
+        Ok(Self {
+            id,
+            map: Arc::new(SkipMap::new()),
+            wal: Some(Wal::create(path.as_ref())?),
+            approximate_size: Arc::new(AtomicUsize::new(0)),
+        })
     }
 
     /// Create a memtable from WAL
-    pub fn recover_from_wal(_id: usize, _path: impl AsRef<Path>) -> Result<Self> {
-        unimplemented!()
+    pub fn recover_from_wal(id: usize, path: impl AsRef<Path>) -> Result<Self> {
+        le map = Arc::new(SkipMap::new());
+        Ok(Self {
+            id,
+            wal: Some(Wal::recover(path.as_ref(), &map)?),
+            map,
+            approximate_size: Arc::new(AtomicUsize::new(0)),
+        })
     }
 
     pub fn for_testing_put_slice(&self, key: &[u8], value: &[u8]) -> Result<()> {
@@ -89,7 +105,7 @@ impl MemTable {
     /// Get a value by key.
     pub fn get(&self, key: &[u8]) -> Option<Bytes> {
         self.map.get(key)
-                                .map(|x| x.value().clone())
+            .map(|x| x.value().clone())
     }
 
     /// Put a key-value pair into the mem-table.
